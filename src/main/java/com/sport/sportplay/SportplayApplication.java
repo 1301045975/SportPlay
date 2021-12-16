c56f7ab008550f70561e8b58747ef2e81a9f1aa9
王桂林 <1301045975@qq.com>
2021-12-16 21:26:00 +0800

    增删改查

--- src/main/java/com/sport/sportplay/SportplayApplication.java
+++ src/main/java/com/sport/sportplay/SportplayApplication.java
@@ -1,10 +1,12 @@
 package com.sport.sportplay;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.sport.sportplay.dao")
 public class SportplayApplication {
 
     public static void main(String[] args) {
